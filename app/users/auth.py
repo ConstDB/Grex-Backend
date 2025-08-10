8e5a1fe10aa8cc0b16c41a7ed5baace4c82fc808
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-08-11 00:15:42 +0800

    add initial setup for authentication jwt

---
+++ app/users/auth.py
@@ -0,0 +1,46 @@
+import jwt
+from dotenv import load_dotenv
+import os
+import time
+from fastapi import HTTPException
+from passlib.context import CryptContext
+
+load_dotenv()
+
+JWT_SECRET = os.getenv("JWT_SECRET")
+JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
+pwd_content = CryptContext(schemes=["bcrypt"], deprecated=auto)
+
+
+def token_response(token:str):
+    return{
+        "Access_token" : token
+    }
+
+def get_password_hash(password:str):
+    return pwd_content.hash(password)
+
+def verify_password(plain_password:str, hashed_password:str):
+    return pwd_content.verify(plain_password, hashed_password)
+
+
+def signJWT(user_id:str):
+    try:
+        payload = {
+            "id" : user_id,
+            "expires": time.time() + 1200
+        }
+
+        token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+        return token_response(token)
+    except Exception as e:
+        return { "message": f"Failed to encode token -> {e}"}
+
+
+def decodeJWT(token:str):
+    try:
+        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=JWT_ALGORITHM)
+        return decoded_token if decoded_token["expires"] >= time.time() else None
+    except Exception as e:
+        return { "message": f"Failed to decode token -> {e}"}
--- app/users/routes.py
+++ app/users/routes.py
@@ -1,5 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from .schemas import UserLoginSchema, UserRegisterSchema
+from .auth import verify_password, get_password_hash
+from ..db.database import Database
+from .crud import add_user_to_db
+import asyncpg
 
+db = Database()
 
 router = APIRouter()
 
@@ -7,3 +13,17 @@ router = APIRouter()
 @router.get("/testing")
 async def Testing():
     return "hello this is users route"
+
+
+@router.post("/sign-up")
+async def sign_up(user: UserRegisterSchema):
+    user_dict = user.model_dump()
+    password = get_password_hash(user.password)
+    add_user_to_db(user_dict)
+    
+
+    
+
+@router.post("/login")
+async def login(user: UserLoginSchema):
+    pass
