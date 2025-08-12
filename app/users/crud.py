8b44ad2dd726d29c1552753c9b690d208beb702d
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-08-12 23:37:11 +0800

    add login feature

--- app/users/crud.py
+++ app/users/crud.py
@@ -4,9 +4,19 @@ from ..db.database import Database
 
 async def add_user_to_db(user: dict, conn: asyncpg.Connection):
     query = """
-        INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3)
+        INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3) RETURNING *
     """
     
-    res = await conn.fetchval(query, *user.values())
+    res = await conn.fetchrow(query, *user.values())
 
+    return res
+
+
+async def get_user(email:str, conn: asyncpg.Connection):
+    query = """
+        SELECT * FROM users WHERE email = $1
+    """
+
+    res = await conn.fetchrow(query, email)
+    
     return res
