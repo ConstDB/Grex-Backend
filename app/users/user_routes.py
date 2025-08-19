c0d847f1569a9de8042873a9a9d12481257a9931
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-08-19 10:17:30 +0800

    add logout

--- app/users/user_routes.py
+++ app/users/user_routes.py
@@ -4,7 +4,8 @@ from .auth import verify_password, get_password_hash, create_access_token, creat
 from authlib.integrations.starlette_client import OAuth
 from ..db.database import Database
 from ..deps import get_db_connection
-from .crud import add_user_to_db, get_user, update_refresh_token
+from .crud import add_user_to_db, get_user_from_db
+from .auth import get_current_user
 import logging
 import asyncpg
 import os
@@ -18,3 +19,12 @@ logger = logging.getLogger("uvicorn")
 @router.get("/testing")
 async def Testing():
     return "hello this is users route"
+
+
+@router.post("/user/profile/{user_id}")
+async def get_user_profile(user_id:int, token: str = Depends(get_current_user)):
+    try:
+        return "Hello"
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail={e})
