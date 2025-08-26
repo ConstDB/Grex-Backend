e06a7cdcb5f7566166bbae66486e481bc90e7b91
unknown <danfredmartinisip@gmail.com>
2025-08-27 03:03:51 +0800

    Added core error handlers for routers and crud. Added subquery profile_picture for GET in task_Comment

--- app/task/crud/task_comment_crud.py
+++ app/task/crud/task_comment_crud.py
@@ -1,9 +1,10 @@
 from app.task.schemas.TaskComment_schema import TaskCommentCreate, TaskCommentDelete, TaskCommentUpdate
 from datetime import datetime
-
-# Comment on a task
+from app.core.decorators import db_error_handler
 from datetime import datetime
 
+# Comment on a task
+@db_error_handler
 async def create_taskcomment(conn, task_id: int, taskcomment: TaskCommentCreate):
     created_at = datetime.now()
     query = """
@@ -20,16 +21,26 @@ async def create_taskcomment(conn, task_id: int, taskcomment: TaskCommentCreate)
     return dict(row)
 
 # View comments in a task
+@db_error_handler
 async def get_taskcomment(conn, task_id: int):
     query = """
-        SELECT *
-        FROM task_comments
-        WHERE task_id = $1
-        """
+        SELECT
+            tc.comment_id,
+            tc.task_id,
+            tc.content,
+            tc.sender_id,
+            tc.created_at,
+            u.profile_picture
+        FROM task_comments tc
+        LEFT JOIN users u ON u.user_id = tc.sender_id
+        WHERE tc.task_id = $1
+        ORDER BY tc.created_at ASC;
+    """
     rows = await conn.fetch(query, task_id)
     return [dict(r) for r in rows]
 
 # Editing a comment
+@db_error_handler
 async def update_taskcomment(conn, task_id: int, comment_id: int, taskcomment: TaskCommentUpdate):
     query = """
         UPDATE task_comments
@@ -41,6 +52,7 @@ async def update_taskcomment(conn, task_id: int, comment_id: int, taskcomment: T
     return dict(row) if row else None
 
 # Delete comment from task
+@db_error_handler
 async def delete_taskcomment(conn, comment_id: int, task_id: int):
     query = "DELETE FROM task_comments WHERE comment_id = $1 AND task_id = $2 RETURNING *;"
     row = await conn.fetchrow(query, comment_id, task_id)
