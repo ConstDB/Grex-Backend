95c9456fe0fcb110e93d1e426d792dd9c5404ddd
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-08-27 15:25:12 +0800

    add search user endpoint

--- app/users/user_routes.py
+++ app/users/user_routes.py
@@ -4,7 +4,7 @@ from .auth import verify_password, get_password_hash, create_access_token, creat
 from authlib.integrations.starlette_client import OAuth
 from ..db.database import Database
 from ..deps import get_db_connection
-from .crud import add_user_to_db, get_user_from_db
+from .crud import add_user_to_db, get_user_from_db, get_users_by_name
 from .auth import get_current_user
 from ..utils.logger import logger
 import asyncpg
@@ -24,3 +24,13 @@ async def get_user_profile(user_id:int, token: str = Depends(get_current_user)):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail={e})
+
+@router.get("/users/search")
+async def search_users(name:str, conn: asyncpg.Connection = Depends(get_db_connection)):
+    try:
+        users = await get_users_by_name(name, conn)
+        if users is None:
+            raise HTTPException(status_code=404, detail=f"There's no users found with name {name}.")
+        return users
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to search for users -> {e}")
