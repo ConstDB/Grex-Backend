c58208fbf921b3299f56efab5d6b015e671a671f
DenJiwoo <danfredmartinisip@gmail.com>
2025-08-29 19:00:44 +0800

    Created endpoint for categories

--- app/task/routes/task_assignment_router.py
+++ app/task/routes/task_assignment_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from ...deps import get_db_connection
+from ...users.auth import get_current_user
 from app.task.crud import task_assignment_crud
 from app.task.schemas.TaskAssignment_schema import TaskAssignmentOut
 from typing import List
@@ -12,6 +13,7 @@ router = APIRouter()
 async def create_taskassignment(
     task_id: int, 
     user_id: int, 
+    token: str = Depends(get_current_user),
     conn: asyncpg.Connection = Depends(get_db_connection)
 ):
     assigned = await task_assignment_crud.create_taskassignment(conn, task_id, user_id)
@@ -26,7 +28,9 @@ async def create_taskassignment(
 
 # Router for getting all assigned users from a task
 @router.get("/task/{task_id}/assignment", response_model=List[TaskAssignmentOut])
-async def get_taskassignment(task_id: int, conn: asyncpg.Connection = Depends(get_db_connection)):
+async def get_taskassignment(task_id: int, 
+                             token: str = Depends(get_current_user),
+                             conn: asyncpg.Connection = Depends(get_db_connection)):
     taskassignment =  await task_assignment_crud.get_taskassignment(conn, task_id)
     if not taskassignment:
         raise HTTPException(status_code=400, detail="No assigned users")
@@ -34,7 +38,10 @@ async def get_taskassignment(task_id: int, conn: asyncpg.Connection = Depends(ge
 
 # Router for removing assigned users from task
 @router.delete("/task/{task_id}/assignment/{user_id}")
-async def delete_taskassignment(task_id: int, user_id: int, conn: asyncpg.Connection = Depends (get_db_connection)):
+async def delete_taskassignment(task_id: int, 
+                                user_id: int, 
+                                token: str = Depends(get_current_user),
+                                conn: asyncpg.Connection = Depends (get_db_connection)):
     remove = await task_assignment_crud.delete_taskassignment(conn, task_id, user_id)
     if not remove:
         raise HTTPException(status_code=400, detail="Failed to remove user")
