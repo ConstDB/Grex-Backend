ba580c60f3ab893e42496480b7dce7d777787a78
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-08-31 19:02:51 +0800

    add qdrant to docker

--- reset-db.sh
+++ reset-db.sh
@@ -1,5 +1,5 @@
-docker compose down -v
+docker compose down
 sudo rm -rf ./data/
 sudo mkdir -p ./data/pgadmin-data
 sudo chown -R 5050:5050 ./data/pgadmin-data
-docker compose up
+docker compose up --build
