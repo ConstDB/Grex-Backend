7c396a444d1bdd590958ef3156c42682b883f2e3
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-09-05 19:20:35 +0800

    configure dependencies

--- app/messages/routes.py
+++ app/messages/routes.py
@@ -10,12 +10,8 @@ from datetime import datetime
 
 router = APIRouter()
 
-@router.get("/testing")
-async def Testing():
-    return "hello this is messaging route"
-
 @router.get("/workspace/{workspace_id}/messages")
-async def get_messages(workspace_id: int, last_id:int=None, conn: asyncpg.Connection = Depends(get_db_connection)):
+async def get_messages(workspace_id: int, last_id:int=None, conn: asyncpg.Connection = Depends(get_db_connection), token:str = Depends(get_current_user)):
     try:
         response = await get_few_messages_from_db(workspace_id, conn, last_id)
         return response
