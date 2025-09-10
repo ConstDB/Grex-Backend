130ebd5192302b62a28dcd907954f145077beed4
Pena, Matthew Dwayne <penamv@students.national-u.edu.ph>
2025-09-10 15:21:41 +0800

    add pinned_messages

---
+++ app/pinned_messages/crud.py
@@ -0,0 +1,64 @@
+
+import asyncpg
+from fastapi import HTTPException, Depends
+from ..deps import get_db_connection
+from ..utils.query_builder import insert_query
+from ..db_instance import db
+from datetime import datetime
+from .schemas import WorkspaceGetPinnedMessage, WorkspacePinnedMessage, WorkspaceRemovePinnedMessage
+
+
+
+async def workspace_remove_pinned_messages(workspace_id:int, message_id:int, conn: asyncpg.Connection ):
+    try:
+
+        query = """
+        DELETE FROM pinned_messages
+        where workspace_id = $1 
+            AND message_id = $2
+        RETURNING *;
+        """
+        res = await conn.fetchrow(query, workspace_id, message_id )
+
+        return res 
+    except Exception as e: 
+
+        raise HTTPException (status_code=500, detail=f"Process failed -> {e}")
+    
+
+
+async def update_pinned_message(workspace_id: int, model: WorkspaceGetPinnedMessage, conn: asyncpg.Connection):
+    try: 
+        pinned_at = datetime.utcnow()
+        message_id = model.message_id
+        pinned_by = model.pinned_by
+        
+        query = """
+        UPDATE pinned_messages 
+        SET pinned_at = $1
+        WHERE workspace_id = $2 
+        AND message_id = $3
+        AND pinned_by = $4
+        RETURNING *; 
+        """   
+
+        res = await conn.fetchrow(query, pinned_at, model.workspace_id, model.message_id, model.pinned_by)
+        return res 
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"process failed -> {e}")
+
+    
+    
+async def workspace_pinned_messages(workspace_id:int,  conn: asyncpg.Connection):
+    try:
+
+        query = """
+        SELECT * FROM  pinned_messages 
+        WHERE workspace_id = $1
+        ORDER BY pinned_at DESC;
+        """
+
+        res = await conn.fetch (query, workspace_id)
+        return res
+    except Exception as e:
+        raise HTTPException (status_code=500, detail=f"process failed -> {e}")
