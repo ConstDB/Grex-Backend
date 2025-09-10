a3e73f0e9bea4945d2740fc29ed5c134ae012119
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-09-10 23:23:33 +0800

    fine-tuned query_classifier, and set up grex-distilbert

--- app/ai/query_vdb.py
+++ app/ai/query_vdb.py
@@ -1,31 +1,12 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
-import torch
+from transformers import pipeline
 
-# quant_config = BitsAndBytesConfig(
-#     load_in_4bit=True,
-#     bnb_4bit_use_double_quant=True,
-#     bnb_4bit_quant_type="nf4",           # nf4 is usually best for LLMs
-#     bnb_4bit_compute_dtype=torch.float16 # keep compute in fp16
-#     )
-model = AutoModelForCausalLM.from_pretrained(
-    "microsoft/phi-1_5",
-    dtype=torch.float16,
-    # quantization_config=quant_config,
-    device_map="auto",
-    low_cpu_mem_usage=True
-    )
-tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5")
+model_name = "model/grex-distilbert"
+classifier = pipeline("text-classification", model=model_name, tokenizer=model_name)
 
+def query_classifier(query):
+    result = classifier(query)
+    predicted_class = result[0]["label"]
+    return int(predicted_class.split("_")[1])
 
-prompt = """
-Query: "@GrexAI what is the synonym of good"
-"""
-
-inputs = tokenizer(prompt, return_tensors="pt")
-output = model.generate(
-    **inputs,
-    max_new_tokens=30,
-    do_sample=False,     # deterministic output
-)
-
-print(tokenizer.decode(output[0], skip_special_tokens=True))
+query = "@GrexAI do you think this is the right solution for these tasks?"
+print(f"Choice: {query_classifier(query)}")
