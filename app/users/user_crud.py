e5070a28cc69acb6d5d9d72c665ca6d94ed28bc0
Pena, Matthew Dwayne <penamv@students.national-u.edu.ph>
2025-09-13 01:26:10 +0800

    modified code corrections according to the liking of the supreme commanda

--- app/users/user_crud.py
+++ app/users/user_crud.py
@@ -2,53 +2,7 @@ from fastapi import Depends, HTTPException
 import asyncpg
 from asyncpg.exceptions import UniqueViolationError
 from ..utils.query_builder import insert_query, get_query, update_query
-from ..utils.logger import logger
 
-async def add_user_to_db(user: dict, conn: asyncpg.Connection):
-    # query = """
-    #     INSERT INTO users (first_name, last_name, email, password_hash, phone_number) VALUES ($1, $2, $3, $4, $5) RETURNING *
-    # """
-    try:
-        query = insert_query(user, table="users", returning="user_id, first_name, last_name, email, profile_picture, phone_number, status")
-        
-        res = await conn.fetchrow(query, *user.values())
-
-        return res
-    except UniqueViolationError as e:
-        raise HTTPException(status_code=400, detail="A user with that email already exists.")
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Something went wrong -> {e}")
-
-async def get_user_from_db(email:str, conn: asyncpg.Connection, fetch:str="*"):
-    # query = """
-    #     SELECT * FROM users WHERE email = $1
-    # """
-
-    try:
-
-        query = get_query("email", fetch=fetch, table="users")
-        res = await conn.fetchrow(query, email)
-
-        return res
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Something went wrong -> {e}")
-
-async def update_refresh_token_on_db(user_id:int, payload:dict, conn: asyncpg.Connection):
-    try: 
-        query = update_query("user_id", model=payload, table="users")
-        return await conn.execute(query, *payload.values(), user_id)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Something went wrong -> {e}")
-
-async def revoke_user_token_on_db(user_id:int, payload:dict, conn: asyncpg.Connection):
-    try:
-        query = update_query("user_id", model=payload, table="users")
-        logger.info(query)
-        await conn.execute(query, *payload.values(), user_id)
-        return {"message": "Successful Logout"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Something went wrong on CRUD -> {e}")
 
 async def get_users_by_name(name: str, conn: asyncpg.Connection):
     try:
@@ -63,11 +17,13 @@ async def get_users_by_name(name: str, conn: asyncpg.Connection):
         return res
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to search user on DB -> {e}")
+    
 
 async def get_user_data_db(user_id: int, conn: asyncpg.Connection ):
     try: 
         query = """
-        SELECT * FROM users
+        SELECT first_name, last_name, email, phone_number, profile_picture
+        FROM users
         WHERE user_id = $1
         """    
     
@@ -131,8 +87,3 @@ async def update_user_information_db(
     
     user = await conn.fetchrow (query, *user_values)
     return dict(user) if user else None
-
-   
-
-        
-    
--- app/users/user_routes.py
+++ app/users/user_routes.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
-from .schemas import UserLoginSchema, UserRegisterSchema, RefreshToken,GetUserData
+from .schemas import UserLoginSchema, UserRegisterSchema, RefreshToken,PatchUserResponse,GetUserResponse
 from .auth import verify_password, get_password_hash, create_access_token, create_refresh_token, token_response, oauth, decode_refresh_token
 from authlib.integrations.starlette_client import OAuth
 from ..db.database import Database
 from ..deps import get_db_connection
-from .crud import add_user_to_db, get_user_from_db, get_users_by_name,get_user_data_db,update_user_information_db
+from .authentication_crud import add_user_to_db, get_user_from_db
+from .user_crud import get_users_by_name,get_user_data_db,update_user_information_db
 from .auth import get_current_user
 from ..utils.logger import logger
 from ..utils.normalizer import normalize_name
@@ -12,18 +13,6 @@ import asyncpg
 import os
 
 router = APIRouter()
-
-@router.get("/testing")
-async def Testing():
-    return "hello this is users route"
-
-
-@router.post("/user/profile/{user_id}")
-async def get_user_profile(user_id:int, token: str = Depends(get_current_user)):
-    try:
-        return "Hello"
-    except Exception as e:
-        raise HTTPException(status_code=500, detail={e})
 
 @router.get("/users/search")
 async def search_users(name:str, conn: asyncpg.Connection = Depends(get_db_connection), token: str = Depends(get_current_user)):
@@ -35,21 +24,20 @@ async def search_users(name:str, conn: asyncpg.Connection = Depends(get_db_conne
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to search for users -> {e}")
 
-@router.get("/user/{user_id}/profile")
-async def fetch_user_data_route(user_id: int, conn: asyncpg.Connection = Depends(get_db_connection)):
+@router.get("/user/{user_id}/profile", response_model = GetUserResponse)
+async def get_user_data_route(user_id: int, conn: asyncpg.Connection = Depends(get_db_connection), token: str = Depends(get_current_user)):
     try:
         users = await get_user_data_db(user_id, conn)
         
-        return users
+        return dict (users)
     except Exception as e: 
         raise HTTPException (status_code=500, detail=f"Failed to get User Data -> {e}")
     
 @router.patch("/user/{user_id}/profile")
-async def update_user_info_route(user_id:int, model: GetUserData, conn: asyncpg.Connection = Depends(get_db_connection)):
+async def update_user_info_route(user_id:int, model: PatchUserResponse, conn: asyncpg.Connection = Depends(get_db_connection), token: str = Depends(get_current_user)):
     try: 
         users = await update_user_information_db(user_id, model.model_dump(), conn)
          
         return users
     except Exception as e:
         raise HTTPException(status_code=500, detail = f"Process Failed -> {e}")
-    
