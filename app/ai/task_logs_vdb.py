ca51140f55db0c1d6ff65659e9e0119417911687
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-09-12 17:04:43 +0800

    add queue algorithm on processing tasklog into embedding

--- app/ai/task_logs_vdb.py
+++ app/ai/task_logs_vdb.py
@@ -3,42 +3,71 @@ from qdrant_client.models import PointStruct
 from ..utils.logger import logger
 from .qdrant_config import qdrant, TASKS_COLLECTION_NAME
 import asyncio
-
-task = TASKS_COLLECTION_NAME
-
-task_logs = "Leader None patched task_id 1. Changes: title changed from 'example' to 'string'"
+from collections import deque
 
 
-async def insert_task_logs_to_vdb(task_log_id:int, workspace_id:int, content:str):
-    try:
-        embedding = compute_embedding(content)
-        point = PointStruct(id=task_log_id, vector=embedding, payload={"task_log_id": task_log_id, "workspace_id": workspace_id})
-        res = await qdrant.upsert(collection_name=task, points=[point]) 
-        print(res)
-    except Exception as e:
-        # logger.info(f"Failed to insert user to Qdrant -> {e}")
-        print(f"Failed to insert user to Qdrant -> {e}")
+
+class ProcessTaskLog:
+
+    def __init__(self):
+        self.task = TASKS_COLLECTION_NAME
+        self.task_logs_queue= deque()
 
 
-# asyncio.run(insert_task_logs_to_vdb(1,1, task_logs))
+    async def insert_task_logs_to_vdb(self, task_log_id:int, workspace_id:int, content:str):
+        try:
+            embedding = compute_embedding(content)
+            point = PointStruct(id=task_log_id, vector=embedding, payload={"task_log_id": task_log_id, "workspace_id": workspace_id})
+            res = await qdrant.upsert(collection_name=self.task, points=[point]) 
+            print(res)
+        except Exception as e:
+            # logger.info(f"Failed to insert user to Qdrant -> {e}")
+            print(f"Failed to insert user to Qdrant -> {e}")
 
 
-async def get_task_embeddings():
-    try:
-        records, _ = await qdrant.scroll(
-            collection_name=task,
-            limit=10,
-            with_payload=True,
-            with_vectors=True
-        )
+    # asyncio.run(insert_task_logs_to_vdb(1,1, task_logs))
 
+
+    async def get_task_embeddings(self):
+        try:
+            records, _ = await qdrant.scroll(
+                collection_name=self.task,
+                limit=10,
+                with_payload=True,
+                with_vectors=True
+            )
+
+            
+            for r in records:
+                print(f"id : {r.id}")
+                print(f"vector : {r.vector}")
+                print(f"payload : {r.payload}")
+        except Exception as e:
+            print(f"Failed to fetch task logs embeddings -> {e}")
+
+    async def insert_data(self,task_log_id:int, workspace_id:int, content:str ):
+        data = {
+            "task_log_id": task_log_id, "workspace_id": workspace_id, "content": content
+        }
+        self.task_logs_queue.append(data)
         
-        for r in records:
-            print(f"id : {r.id}")
-            print(f"vector : {r.vector}")
-            print(f"payload : {r.payload}")
-    except Exception as e:
-        print(f"Failed to fetch task logs embeddings -> {e}")
+        if not self.task_logs_queue:
+            self.queue(data)
 
 
-asyncio.run(get_task_embeddings())
+
+    async def queue(self, data: dict):
+        log = self.task_logs_queue.pop()
+        await self.insert_task_logs_to_vdb(log["task_log_id"], log["workspace_id"], log["content"])
+
+        if self.task_logs_queue:
+            self.queue()
+        else:
+            logger.info("Queue is empty")
+
+            
+        
+
+t = ProcessTaskLog()
+
+asyncio.run(r.get_task_embeddings())
