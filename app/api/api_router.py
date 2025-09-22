80cbf90afa15fbe0a0389df8609436b675f248a2
Pena, Matthew Dwayne <penamv@students.national-u.edu.ph>
2025-09-22 19:49:18 +0800

    modified changes are done according to the likings of the supreme commander for  quicklinks endpoints

--- app/api/api_router.py
+++ app/api/api_router.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 from ..messages.routes import router as messages_router
 from ..notifications.routes.notif_router import router as notification_router
 from ..notifications.routes.notif_recipient_router import router as notification_recipient_router
-# from ..summaries.routes import router as summaries_router
 from ..categories.routes import router as category_router
 from ..task.routes.task_router import router as task_router
 from ..task.routes.task_attachment_router import router as task_attachment_router
@@ -14,26 +13,22 @@ from ..authentication.routes import router as auth_router
 from ..workspaces.routes import router as workspaces_router
 from ..messages.websocket import router as websocket_router
 from ..pinned_messages.routes import router as pinned_router  
-
-
+from ..quick_links.routes import router as link_router
 
 router = APIRouter()
 
-
 router.include_router(messages_router, tags=["Messages"])
-router.include_router(notification_router, prefix="/notifications", tags=["Notifications"])
+router.include_router(notification_router, prefix="/notification", tags=["Notification"])
 router.include_router(notification_recipient_router, prefix="/notification-recipients", tags=["Notification Recipients"])
-# router.include_router(summaries_router, prefix="/summaries", tags=["Summaries"])
 router.include_router(category_router, tags=["Category"])
 router.include_router(task_router, prefix="/tasks", tags=["Task"])
 router.include_router(task_attachment_router, tags=["Task Attachment"])
-router.include_router(task_comment_router, tags=["Task Comment"])
+router.include_router(task_comment_router, tags=["TaskComment"])
 router.include_router(sub_task_router, tags=["SubTask"])
-router.include_router(task_assignment_router, tags=["Task Assignment"])
+router.include_router(task_assignment_router, tags=["TaskAssignment"])
 router.include_router(auth_router, tags=["Authentication"])
 router.include_router(users_router, tags=["Users"])
 router.include_router(workspaces_router, tags=["Workspaces"])
 router.include_router(websocket_router)
 router.include_router(pinned_router, tags=["Pinned Messages"])
-
-
+router.include_router(link_router, tags=["Quick Links"])
---
+++ app/quick_links/routes.py
@@ -0,0 +1,72 @@
+import asyncpg
+from fastapi import APIRouter, Depends, HTTPException
+from typing import List
+from datetime import date
+from ..deps import get_db_connection
+from .crud import fetch_workspace_link_db, remove_workspace_link_db, insert_workspace_links_db, update_link_db
+from ..authentication.services import get_current_user
+from .schemas import GetLinks, CreateLinks, PutLink
+
+
+
+
+router = APIRouter()
+
+@router.put("/workspaces/{workspace_id}/quick-links/{link_id}")
+async def update_workspace_link_route(
+    workspace_id:int,
+    link_id:int,
+    UpdateLink: PutLink,
+    conn:asyncpg.Connection=Depends(get_db_connection),
+    token:str=Depends(get_current_user)
+):
+    try:
+
+        res = await update_link_db(workspace_id, link_id, UpdateLink, conn)
+        return res
+    except Exception as e: 
+        raise HTTPException(status_code=500, detail=f"process failed -> {e}")
+
+
+@router.post("/workspaces/{workspace_id}/quick-links",response_model=GetLinks)
+async def add_workspace_links_route(
+    workspace_id:int, 
+    link:CreateLinks,
+    conn:asyncpg.Connection = Depends(get_db_connection),
+    token:str = Depends(get_current_user)
+):
+    try:
+
+
+        res = await insert_workspace_links_db(workspace_id, link, conn)
+        return res
+    except Exception as e: 
+        raise HTTPException(status_code=500, detail=f"process failed -> {e}")
+
+
+@router.get("/workspaces/{workspace_id}/quick-links", response_model=List[GetLinks])
+async def get_workspace_links_route(
+workspace_id: int,                              
+conn:asyncpg.Connection=Depends(get_db_connection),
+    token: str = Depends(get_current_user)):  
+    try:
+
+        res = await fetch_workspace_link_db(workspace_id, conn)
+        return res 
+    except Exception as e:
+        raise HTTPException(status_code = 500, detail =f"process ay mali -> {e}")
+
+
+@router.delete("/quick-links/{link_id}")
+async def delete_workspace_link_route(
+    link_id:int, conn:asyncpg.Connection = Depends(get_db_connection),
+    token:str = Depends(get_current_user)):
+    try:
+
+        res = await remove_workspace_link_db(link_id, conn)
+        return res
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"process failed -> {e}")
+
+
+
